f93ddd5fc9cae8b59c22e9057bddb96277820d25
uaSad <uaSad.n5rvqfxt@gmail.com>
2014-08-30 17:43:07 +0600

    no message

--- untitled_auth/js/main.js
+++ untitled_auth/js/main.js
@@ -1,40 +1,43 @@
 $(document).ready(function () {
-    $('form').submit(function() {
-        /*if ($(this).find('input[name=name]').val() == '') {
-            alert('Введите имя пользователя');
-            return false;
+    var showGreeting = function() {
+        var d = new Date();
+        var h = d.getHours();
+        var greet;
+
+        if (h >= 6 && h < 12) {
+            greet = 'Доброе утро';
         }
-        else if ($(this).find('input[name=password]').val() == '') {
-            alert('Введите пароль');
+        else if (h >= 12 && h < 18) {
+            greet = 'Добрый день';
+        }
+        else {
+            greet = 'Добрый вечер';
+        }
+
+        $('<div id="greeting"></div>').
+            appendTo('#wrapper').
+            css('opacity', '0').
+            append('<h3>'+ greet +'!</h3>').
+            animate({ opacity: '1' }, 1000).
+            click(function() {
+                $(this).remove();
+            });
+    };
+
+    $('form').submit(function() {
+        var userName = $(this).find('input[name=name]').val();
+        var userPassword = $(this).find('input[name=password]').val();
+
+        if (!userName || !userPassword)
             return false;
-        }*/
-        if ($(this).find('input[name=name]').val() == 'user' &&
-                $(this).find('input[name=password]').val() == 'password') {
+
+        if (userName.toLowerCase() == 'user' &&
+                userPassword == 'password') {
             $('.form-signin-container').animate(
-                { opacity: "0" },
-                1000,
+                { opacity: '0' }, 1000,
                 function() {
-                    /*$(this).css({display:'none'});*/
                     $(this).remove();
-
-                    var d = new Date();
-                    var h = d.getHours();
-                    var greet;
-
-                    if (h >= 6 && h < 12) {
-                        greet = 'Доброе утро';
-                    }
-                    else if (h >= 12 && h < 18) {
-                        greet = 'Добрый день';
-                    }
-                    else {
-                        greet = 'Добрый вечер';
-                    }
-
-                    $('#greeting').css('opacity', '0')
-                        .html('<h3>'+ greet +'!</h3>')
-                        .animate({ opacity: "1" }, 1000);
-
+                    showGreeting();
                 }
             );
             /* без сервера */
